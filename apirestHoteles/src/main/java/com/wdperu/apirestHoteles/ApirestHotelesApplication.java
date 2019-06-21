b212b278aa7a5f0fc5781edf3bbecdadf1d231dd
ronaldo <barzolaronaldog@gmail.com>
2019-06-21 16:58:13 -0500

    Configuracion para despliegue de api Hoteles

--- apirestHoteles/src/main/java/com/wdperu/apirestHoteles/ApirestHotelesApplication.java
+++ apirestHoteles/src/main/java/com/wdperu/apirestHoteles/ApirestHotelesApplication.java
@@ -5,16 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 @SpringBootApplication
-public class ApirestHotelesApplication /*extends SpringBootServletInitializer*/{
+public class ApirestHotelesApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApirestHotelesApplication.class, args);
 	}
 	
-//
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//		return application.sources(ApirestHotelesApplication.class);
-//	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApirestHotelesApplication.class);
+	}
 
 }
